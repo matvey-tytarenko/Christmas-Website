92b264c2d56f34b09bf9670c06460328591f80ec
“Matvey <matveytytarenko25@gmail.com>
2024-12-09 16:38:47 +0100

    change

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const router = require("./Routes/UserRouter");
-const CorsControl = require('./Controller/CorsControl')
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(CorsControl);
 app.use("/api", router);
 
 module.exports = app;
