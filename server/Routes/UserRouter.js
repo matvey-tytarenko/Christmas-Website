ac65df6702739b5181c2554a528fe5ba16e78aa4
“Matvey <matveytytarenko25@gmail.com>
2024-12-24 18:41:58 +0100

    ф

--- server/Routes/UserRouter.js
+++ server/Routes/UserRouter.js
@@ -1,10 +1,11 @@
-const { GetMail } = require('../Controller/UserController');
+const { GetMail } = require("../Controller/UserController");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
 router.get("/congratulation", (req, res) => {
-    router.post("/congratulation", GetMail);
-    return res.status(200).json({msg: "Email was sent!"});
-})
+  return res.status(200).json({ msg: "Email was sent!" });
+});
+
+router.post("/congratulation", GetMail);
 
 module.exports = router;
