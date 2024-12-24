b6dc92a019678d98fce03e2b9878c9d328faa6c0
“Matvey <matveytytarenko25@gmail.com>
2024-12-24 18:32:41 +0100

    change API

--- client/happy-new-year/src/api/API.ts
+++ client/happy-new-year/src/api/API.ts
@@ -1,3 +1,7 @@
-const server = "http://localhost:5000/api/congratulation";
+// Localhost
+// const server = "http://localhost:5000/api/congratulation";
+
+// Vercel
+const server = "https://christmas-website-server.vercel.app/api/congratulation";
 
 export default server;
