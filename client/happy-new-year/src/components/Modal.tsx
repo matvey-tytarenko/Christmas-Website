50373a99eb4c792b0b148c9009987fe0767ce090
“Matvey <matveytytarenko25@gmail.com>
2024-12-26 16:03:11 +0100

    link change

--- client/happy-new-year/src/components/Modal.tsx
+++ client/happy-new-year/src/components/Modal.tsx
@@ -9,7 +9,7 @@ const Modal = () => {
       <h3>🎄 У нас появилось приложение! 🎁</h3>
       <p>Скачайте наше приложение для Windows</p>
       <div className="app-modal__buttons">
-        <a href="https://download1529.mediafire.com/kode6yd9cvfgQiJVi-9S5GGgFNLO8GQox7bXG3C0oVba0CTv9fN26AZb-Qt8WCbTj7i492RzHzjOYZTw3jOaami34zuZtDgyuoegktfoHVwLoEY-gheCqLkwc4rolvqw9QDj4MPxidSouugwX88URXn9ZDGrSfdfDFN2-6Lboag/4b4tpdhh74jqfvw/christmass.exe" className="download-button">
+        <a href="https://drive.google.com/file/d/1FymFP1FZo3twB6dyexLbvVKeir0TauRs/view?usp=drive_link" className="download-button">
           Скачать прямо сейчас
         </a>
       </div>
